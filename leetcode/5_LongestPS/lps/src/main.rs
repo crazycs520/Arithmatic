4c45adfac70434dfec75e830827884ea85c249af
crazycs520 <crazycs520@gmail.com>
2019-01-02 21:33:21 +0800

    rust

--- leetcode/5_LongestPS/lps/src/main.rs
+++ leetcode/5_LongestPS/lps/src/main.rs
@@ -1,30 +1,79 @@
 fn main() {
     let s = longest_palindrome("abaaba");
-    println!("{}",s);
+    println!("{}", s);
 }
 
 fn longest_palindrome(s: &str) -> &str {
     if s.len() == 0 {
-        return ""
+        return "";
     }
-    let mut start: usize  =  0;
-    let mut end: usize = 0;
 
-    for i in 0..s.len()  {
-        let l1 = expand_around_center(s, i, i);
-        let l2 = expand_around_center(s, i, i+1);
+    let mut start: usize = 0;
+    let mut end: usize = 0;
+    let bs = s.as_bytes();
+    for i in 0..bs.len() {
+        let l1 = expand_around_center(bs, i, i);
+        let l2 = expand_around_center(bs, i, i + 1);
         let ll = l1.max(l2);
         if ll > (end - start) {
-            start = i - (ll - 1)/2;
+            start = i - (ll - 1) / 2;
             end = i + ll / 2;
         }
     }
-    return s[start..end+1]
+    return &s[start..end + 1];
 }
-fn expand_around_center(s: &str, mut l: i64,mut r: i64) -> i64 {
-    while l >= 0 && r < s.len() && s[l] == s[r] {
-        l-=1;
-        r+=1;
+fn expand_around_center(s: &[u8], mut l: usize, mut r: usize) -> usize {
+    while l > 0 && r < s.len() && s[l] == s[r] {
+        l -= 1;
+        r += 1;
     }
-    return r-l-1
+    if l == 0 && r < s.len() && s[l] == s[r] {
+        return r - l + 1;
+    }
+    return r - l - 1;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_longest_palindrome() {
+        let cases = test_cases();
+        for case in cases {
+            assert_eq!(case.o, longest_palindrome(case.s));
+        }
+    }
+}
+
+struct Case<'a> {
+    s: &'a str,
+    o: &'a str,
+}
+
+impl<'a> Case<'a> {
+    fn new(ss: [&str; 2]) -> Case {
+        if ss.len() < 2 {
+            return Case { s: "", o: "" };
+        }
+        return Case { s: ss[0], o: ss[1] };
+    }
+    fn new_vec(ss_vec: Vec<[&str; 2]>) -> Vec<Case> {
+        let mut cases: Vec<Case> = Vec::with_capacity(ss_vec.len());
+        for ss in ss_vec {
+            cases.push(Case::new(ss));
+        }
+        return cases;
+    }
+}
+
+fn test_cases() -> Vec<Case> {
+    Case::new_vec(vec![
+        [],
+        ["a", "a"],
+        ["aa", "aa"],
+        ["abaaba", "abaaba"],
+        ["ababa", "ababa"],
+        ["abbd", "bb"],
+    ])
 }
--- leetcode/5_LongestPS/lps/target/debug/deps/lps-77da85531a687461.d
+++
@@ -1,5 +0,0 @@
-/Users/cs/code/arithmatic/leetcode/5_LongestPS/lps/target/debug/deps/lps-77da85531a687461: src/main.rs
-
-/Users/cs/code/arithmatic/leetcode/5_LongestPS/lps/target/debug/deps/lps-77da85531a687461.d: src/main.rs
-
-src/main.rs:
