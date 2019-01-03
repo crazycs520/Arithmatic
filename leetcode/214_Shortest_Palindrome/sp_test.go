3e7ef2b2283398a93e4e47c1d77cd97453b55b1d
crazycs520 <crazycs520@gmail.com>
2019-01-03 20:51:47 +0800

    leetcode 214

---
+++ leetcode/214_Shortest_Palindrome/sp_test.go
@@ -0,0 +1,28 @@
+package sp
+
+import (
+	"testing"
+)
+
+var cases = []struct {
+	in  string
+	out string
+}{
+	{"", ""},
+	{"a", "a"},
+	{"aa", "aa"},
+	{"ab", "bab"},
+	{"aab", "baab"},
+	{"aacecaaa", "aaacecaaa"},
+	{"abcd", "dcbabcd"},
+	{"abb", "bbabb"},
+}
+
+func TestShortestPalindrome(t *testing.T) {
+	for _, c := range cases {
+		re := shortestPalindrome(c.in)
+		if re != c.out {
+			t.Fatalf("in: %v, out: %v, expect: %v\n", c.in, re, c.out)
+		}
+	}
+}
